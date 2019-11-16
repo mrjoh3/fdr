f22122352aea63777432617b50cfcbd837ab8633
Matt Johnson <mrjoh3@gmail.com>
2019-11-16 11:44:26 +1100

    add CFA stations to lookup

--- process_spatial.R
+++ process_spatial.R
@@ -3,9 +3,11 @@
 # processing spatial data
 
 library(sf)
+library(stringr)
 
 cfa_tfb <- st_read('shp/cfa_tfb_district.shp', stringsAsFactors = FALSE) 
 
+# town list
 towns_nm <- st_read('shp/VIC_TOWN_shp.dbf', stringsAsFactors = FALSE) 
 towns <- st_read('shp/VIC_TOWN_POINT_shp.shp', stringsAsFactors = FALSE) %>%
   left_join(towns_nm, by = 'TOWN_PID') %>%
@@ -16,9 +18,33 @@ towns <- st_read('shp/VIC_TOWN_POINT_shp.shp', stringsAsFactors = FALSE) %>%
   mutate(cfa_tfb = tools::toTitleCase(tolower(cfa_tfb)),
          town_val = gsub(' | - ', '-', tolower(unique(town_name)))) %>%
   ungroup() %>%
-  arrange(town_name)
+  arrange(town_name) %>%
+  select(-geometry)
+
   
 
+# CFA stations
 
-saveRDS(towns, 'fdr_fluid/towns.rds')
+stn <- st_read('shp/geomark_point.shp', stringsAsFactors = FALSE) %>%
+  filter(STATE == 'VIC',
+         FEATSUBTYP == 'fire station',
+         !grepl('SATELLITE|&|infrastructure', NAME, ignore.case = TRUE)) %>%
+  select(town_name = NAME_LABEL) %>%
+  mutate(town_name = str_trim(gsub('Fire Station|District', '', town_name), 'both'),
+         town_name = gsub('\\.', '', town_name),
+         town_val = tolower(gsub(' | - ', '-', town_name))) %>%
+  st_join(cfa_tfb) %>%
+  select(town_name,
+         town_val,
+         cfa_tfb = TFB_DIST) %>%
+  mutate(cfa_tfb = tools::toTitleCase(tolower(cfa_tfb)))
+
+st_geometry(stn) <- NULL  
+
+towns2 <- bind_rows(towns, stn) %>%
+  distinct()
+
+
+
+saveRDS(towns2, 'fdr_fluid/towns.rds')
 
