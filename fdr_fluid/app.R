c9a6dbd0b52d10d3fa67732e12c008528fe13eff
Matt Johnson <mrjoh3@gmail.com>
2019-12-01 23:57:26 +1100

    return to cdn

--- fdr_fluid/app.R
+++ fdr_fluid/app.R
@@ -57,20 +57,20 @@ statewide = st_read('https://www.emergency.vic.gov.au/public/osom-geojson.json',
          resources, 
          sizeFmt)
 
-# fontawesomeDep <- htmltools::htmlDependency("fontawesome", "5.9.0",
-#                                           src = c(href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/"),
-#                                           script = "js/fontawesome.min.js", stylesheet = "css/fontawesome.min.css"
-# )
-fontawesomeDep <- htmltools::htmlDependency("fontawesome", "5.11.2",
-                                            src = c(href = "www/"),
-                                            script = "js/fontawesome.js", stylesheet = "css/fontawesome.css"
+fontawesomeDep <- htmltools::htmlDependency("fontawesome", "5.9.0",
+                                          src = c(href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/"),
+                                          script = "js/fontawesome.min.js", stylesheet = "css/fontawesome.min.css"
 )
+# fontawesomeDep <- htmltools::htmlDependency("fontawesome", "5.11.2",
+#                                             src = c(href = "www/"),
+#                                             script = "js/fontawesome.js", stylesheet = "css/fontawesome.css"
+# )
 
 #precis_w <- get_precis_forecast('VIC')
 
 ui <- shinyUI(fluidPage(
   title = 'FDR Weather',
-  responsive = TRUE,
+  #responsive = TRUE,
   theme = shinytheme("superhero"),
   header = NULL,
   useShinydashboard(),
